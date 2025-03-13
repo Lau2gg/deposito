6d836d89d038e877c3918bb9417f027ff599036a
B.fernandez <B.fernandez@DESKTOP-NK6HLB6>
2025-03-13 22:24:31 +0100

    segundo commit

--- Deposito/src/cuentas/CCuenta.java
+++ Deposito/src/cuentas/CCuenta.java
@@ -1,4 +1,10 @@
 package cuentas;
+
+/**
+ * 
+ * @author Lau2gg
+ * 
+ */
 public class CCuenta {
 
 
@@ -51,11 +57,22 @@ public class CCuenta {
 	public void setTipoInterés(double tipoInterés) {
 		this.tipoInterés = tipoInterés;
 	}
+	
+	/** Devuelve el estado de la cuenta
+	 * 
+	 * @return saldo de la cuenta
+	 */
 
 	public double estado()
     {
         return saldo;
     }
+	
+	/**
+	 *Ingresa una cantidad en la cuenta
+	 *@param cantidad cantidad a ingresar
+	 *@throws Exception si la cantidad es negativa
+	 */
 
     public void ingresar(double cantidad) throws Exception
     {
@@ -63,6 +80,12 @@ public class CCuenta {
             throw new Exception("No se puede ingresar una cantidad negativa");
         saldo = saldo + cantidad;
     }
+    
+    /** Retira una cantidad de la cuenta
+     * 
+     * @param cantidad cantidad a retirar
+     * @throws Exception si la cantidad es 0 o negativa
+     */
 
     public void retirar(double cantidad) throws Exception
     {
